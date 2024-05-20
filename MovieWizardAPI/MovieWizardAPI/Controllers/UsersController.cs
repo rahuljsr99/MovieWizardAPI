e25c25a47a1a0fe9ec2a4179a08b4b7aa79155b9
Rahul Kumar <rahulkumar.jsr130299@gmail.com>
2024-05-20 22:50:50 +0530

    Created POST Method and GET method GetUserbyId and AddUser

--- MovieWizardAPI/MovieWizardAPI/Controllers/UsersController.cs
+++ MovieWizardAPI/MovieWizardAPI/Controllers/UsersController.cs
@@ -21,5 +21,29 @@ namespace MovieWizardAPI.Controllers
             var users = await _usersDbContext.Users.ToListAsync();
             return Ok(users);
         }
+        [HttpPost("AddUser")]
+        public async Task<ActionResult> AddUser(Users user)
+        {
+            if(user == null)
+            {
+                return BadRequest();
+            }
+            
+            _usersDbContext.Users.Add(user);
+            await _usersDbContext.SaveChangesAsync();//asynchronously saves changes to the database. It returns the number of state entries written to the database
+            return CreatedAtAction(nameof(GetUserById), new { id = user.UserId }, new { message = "User created successfully", userId = user.UserId });
+        }
+        [HttpGet("GetUserById/{id}")]
+        public async Task<ActionResult<Users>> GetUserById(int id)
+        {
+            var user = await _usersDbContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
     }
 }
