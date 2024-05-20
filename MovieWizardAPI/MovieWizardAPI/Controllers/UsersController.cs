8d72f8790bb16379c13c07858b2c0054b022df6f
Rahul Kumar <rahulkumar.jsr130299@gmail.com>
2024-05-21 03:20:44 +0530

    Added more action methods

--- MovieWizardAPI/MovieWizardAPI/Controllers/UsersController.cs
+++ MovieWizardAPI/MovieWizardAPI/Controllers/UsersController.cs
@@ -33,7 +33,7 @@ namespace MovieWizardAPI.Controllers
             await _usersDbContext.SaveChangesAsync();//asynchronously saves changes to the database. It returns the number of state entries written to the database
             return CreatedAtAction(nameof(GetUserById), new { id = user.UserId }, new { message = "User created successfully", userId = user.UserId });
         }
-        [HttpGet("GetUserById/{id}")]
+        [HttpGet("GetUserById")]
         public async Task<ActionResult<Users>> GetUserById(int id)
         {
             var user = await _usersDbContext.Users.FindAsync(id);
@@ -44,6 +44,47 @@ namespace MovieWizardAPI.Controllers
             }
 
             return user;
+        }
+        [HttpDelete("HardDeleteUser/{id}")]
+        public async Task<ActionResult> HardDeleteUser(int id)
+        {
+            var user = await _usersDbContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var deletedUser = new
+            {
+                UserName = user.UserName,
+                UserId = user.UserId,
+                Email = user.Email
+            };
+
+            _usersDbContext.Users.Remove(user);
+            await _usersDbContext.SaveChangesAsync();
+
+            return Ok(new { message = "User deleted successfully", deletedUser });
+        }
+        [HttpPost("ModifyPermissions")]//to be analyzed and completed (scope: change permissions)
+        public async Task<ActionResult> ModifyPermissions([FromBody]Users user)
+        {
+            var userRecord = await _usersDbContext.Users.FindAsync(user.UserId);
+
+            if (userRecord == null)
+            {
+                return NotFound();
+            }
+            else if(!userRecord.IsActive)
+            {
+                return Ok(new { message = "User is already inactive", userId = userRecord.UserId });
+
+            }
+            userRecord.IsActive = false; // Deactivated by setting IsActive to false
+            await _usersDbContext.SaveChangesAsync();
+
+            return Ok(new { message = "User deactivated successfully", userId = userRecord.UserId });
         }
     }
 }
